8e760312216348bb85079e715cb0eb68d54d9c30
MUHAMMAD MUNEEB AHMAD <muneebahmad.net@gmail.com>
2023-05-13 15:40:23 +0500

    add sign up screen

--- src/component/singup.jsx
+++ src/component/singup.jsx
@@ -1,110 +1,35 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import { Card, CardContent } from "@mui/material";
+import { Column, Heading, Stack, Row, Input, Button } from "native-base";
 import { Link } from "react-router-dom";
-export default function Singup() {
+import React from "react";
+
+function Singup() {
   return (
-    <Box>
-      <Grid
-        // height={window.innerHeight}
-        container
-        flexWrap={"wrap"}
-        alignItems="center"
-        justifyContent="center"
-        direction="row"
-        marginTop={20}
-      >
-        <Grid item>
-          <Card sx={{ bgcolor: "", alignContent: "center", maxWidth: 600 }}>
-            <Container component="main" maxWidth="xs">
-              <CssBaseline />
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <CardContent>
-                  <Box component="form" noValidate sx={{ mt: 1 }}>
-                    <Box
-                      container
-                      alignContent={"center"}
-                      justifyContent={"center"}
-                    >
-                      <Box
-                        sx={{
-                          justifyContent: "center",
-                        }}
-                      >
-                        <Grid item ml={15}>
-                          <h2>
-                            SING UP
-                            <br />
-                            ---
-                          </h2>
-                        </Grid>
-                      </Box>
-                    </Box>
-                    <Grid container>
-                      <Grid item sx={{ mr: 1 }}>
-                        <TextField
-                          margin="normal"
-                          type="text"
-                          id="fname"
-                          label="First Name"
-                          name="fname"
-                        />
-                      </Grid>
-                      <Grid item xs>
-                        <TextField
-                          margin="normal"
-                          type="text"
-                          id="Lname"
-                          label="Last Name"
-                          name="Lname"
-                        />
-                      </Grid>
-                    </Grid>
-                    <TextField
-                      margin="normal"
-                      required
-                      fullWidth
-                      name="Email"
-                      label="Your Email"
-                      type="Email"
-                      id="email"
-                    />
-                    <TextField
-                      margin="normal"
-                      required
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      id="password"
-                    />
-                    <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      sx={{ mt: 3, mb: 2 }}
-                    >
-                      Sign Up
-                    </Button>
-                    <Link to={"/"}>{"Already have an account?"}</Link>
-                  </Box>
-                </CardContent>
-              </Box>
-            </Container>
-          </Card>
-        </Grid>
-      </Grid>
-    </Box>
+    <Stack alignItems={"center"}>
+      <Column marginTop={20} space={4} width={500} backgroundColor={""}>
+        <Stack alignItems={"center"}>
+          <Row space={4}>
+            <Heading>SIGN up</Heading>
+          </Row>
+          <Row>
+            <Link to={"/"}>Already have an account?</Link>
+          </Row>
+        </Stack>
+        <Row space={2}>
+          <Column width={"48%"}>
+            <Input mx="3" placeholder="First Name" w="100%" />
+          </Column>
+          <Column width={"45%"}>
+            <Input mx="3" placeholder="Last Name" w="100%" />
+          </Column>
+        </Row>
+        <Input mx="3" placeholder="Email" />
+        <Input mx="3" placeholder="Password" />
+        <Row>
+          <Button w={"100%"}>Sing up</Button>
+        </Row>
+      </Column>
+    </Stack>
   );
 }
+
+export default Singup;
