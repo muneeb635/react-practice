5b5e323621141d6a39841f0ed59340c1548a9ae7
MUHAMMAD MUNEEB AHMAD <muneebahmad.net@gmail.com>
2023-05-24 10:38:05 +0500

    ok

---
+++ src/component/customButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
+ const ButtonTheme=createTheme({
+    
+ })
+function CustomButton() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CustomButton
